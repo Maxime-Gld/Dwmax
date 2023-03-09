ed8c2f542e93cdf9126e6b12e52293dc1ac9e9b8
Maxime-Gld <Maxime.Gdev@hotmail.com>
2023-03-09 21:16:17 +0100

    correction balide i

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const Card = (icon, title) => {
-  return <div>
-    <i><{icon}/i>
-    <h2>{title}</h2>
-  </div>;
+  return (
+    <div>
+      <i>{icon}</i>
+      <h2>{title}</h2>
+    </div>
+  );
 };
 
 export default Card;
