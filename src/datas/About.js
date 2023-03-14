e6bda49629be45dae5608fd3da72e1237fc16fa5
Maxime-Gld <Maxime.Gdev@hotmail.com>
2023-03-14 18:40:36 +0100

    ajout assets et data

---
+++ src/datas/About.js
@@ -0,0 +1,9 @@
+const data = 
+    {
+    name:  "<name> Galonde </name>",
+    surname: "<surname>Maxime</surname>",
+    age: "<age>getFullYear() - 1992</age>",
+    trade: "<trade>Developpeur Web</trade>"
+};
+
+export default data;
